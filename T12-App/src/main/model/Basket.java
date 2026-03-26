1c5101d8a8cb817682648f9138a842509122b9e5
MorningStar572 <Sogo.Oyewo@city.ac.uk>
2026-03-26 22:02:56 +0000

    Update Basket.java

--- T12-App/src/main/model/Basket.java
+++ T12-App/src/main/model/Basket.java
@@ -1,4 +1,25 @@
 package main.model;
 
 public class Basket {
+   private List<OrderLine> items = new ArrayList<>();
+
+    public void addItem(Product product, int quantity) {
+        items.add(new OrderLine(product, quantity));
+    }
+
+    public void removeItem(Product product) {
+        items.removeIf(item -> item.getProduct().equals(product));
+    }
+
+    public List<OrderLine> getItems() {
+        return items;
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (OrderLine item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
 }
