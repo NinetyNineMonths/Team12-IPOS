eeb75f5f89921fd079d5920815fecdf2bf1b8669
MorningStar572 <Sogo.Oyewo@city.ac.uk>
2026-03-26 22:06:21 +0000

    Update Order.java

--- T12-App/src/main/model/Order.java
+++ T12-App/src/main/model/Order.java
@@ -1,4 +1,33 @@
 package main.model;
 
 public class Order {
+  private String orderId;
+    private List<OrderLine> items;
+    private double total;
+
+    public Order(String orderId, List<OrderLine> items) {
+        this.orderId = orderId;
+        this.items = items;
+        calculateTotal();
+    }
+
+    private void calculateTotal() {
+        total = 0;
+        for (OrderLine item : items) {
+            total += item.getPrice();
+        }
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public List<OrderLine> getItems() {
+        return items;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+}
 }
