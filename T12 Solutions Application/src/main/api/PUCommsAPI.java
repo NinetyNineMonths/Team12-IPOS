da20f0b870bb69cdcf08155189d406d1220032b4
fathiatayodele <fathiat.ayodele@city.ac.uk>
2026-04-04 15:37:33 +0100

    Added PUCommsAPI so Impl can run

--- T12 Solutions Application/src/main/api/PUCommsAPI.java
+++ T12 Solutions Application/src/main/api/PUCommsAPI.java
@@ -1,4 +1,10 @@
 package main.api;
 
-public class PUCommsAPI {
+public interface PUCommsAPI {
+
+    boolean sendEmail(String to, String subject, String body);
+
+    boolean authorisePayment(String orderId, double amount);
+
+    void recordTransaction(String refId, String type, String outcome, String timestamp);
 }
